447d3cadeda04c23222c096f8d4730f867a07779
Damilola Adebowale <d.oluwadamilola@zarttech.com>
2024-08-01 12:35:46 +0100

    getters and actions

--- pinia/src/App.vue
+++ pinia/src/App.vue
@@ -1,13 +1,59 @@
 <template>
   <div>
     <header>
-      <h1>Pinia</h1>
+      <h1>Pinia Task</h1>
     </header>
+
+    <div class="new-task-form">
+      <TaskForm/>
+    </div>
+
+       <!-- filter -->
+
+    <nav class="filter">
+      <button @click="filter='all'">All Tasks</button>
+      <button @click="filter='favs'">Fav Tasks</button>
+
+    </nav>
+
+    <!-- task list -->
+
+    <div class="task-list" v-if="filter ==='all'">
+      <p>You have {{ taskStore.totalCount }} task left to do</p>
+      <div v-for="task in taskStore.tasks">
+        <TasksDetails :task="task"/>
+
+      </div>  
+    </div>
+
+ 
+
+    <div class="task-list" v-if="filter ==='favs'">
+           <p>You have {{ taskStore.favCount }} favs left to do</p>
+      <div v-for="task in taskStore.Favs">
+        <TasksDetails :task="task"/>
+
+      </div>  
+    </div>
+    
   </div>
 </template>
 
 <script>
+import {ref} from 'vue'
+  import {useTaskStore} from './stores/taskStore.js'
+  import TasksDetails from './components/TasksDetails.vue'
+  import TaskForm from './components/TaskForm.vue'
+  
   export default {
+    components :{TasksDetails, TaskForm},
+   
+    setup(){
+      const taskStore = useTaskStore()
+
+      const filter = ref('all')
+      return {taskStore, filter}
+    }
     
   }
 </script>
--- pinia/src/stores/taskStore.js
+++ pinia/src/stores/taskStore.js
@@ -4,9 +4,37 @@ export const useTaskStore = defineStore('taskStore', {
 
     state:()=>({
         tasks:[
-            {id:1, title:'buy milk', Fav: false},
-            {id:2, title:'Cook', Fav: true}
+            {id:1, title:'buy milk', isFav: false},
+            {id:2, title:'Learn vue Pinia', isFav: true}
         ],
-        name:'Dami'
-    })
+    }),
+    // getters get the state and compute values based on the state in the store
+    getters:{ 
+        Favs: (state)=>{
+            return state.tasks.filter(task =>task.isFav)
+        },
+        favCount:(state)=>{
+                return state.tasks.reduce((previous, current)=>{
+                    return current.isFav? previous + 1 : previous
+                },0)
+        },
+        totalCount(){
+          return  this.tasks.length
+        }
+    },
+    actions:{
+        addTask(task){
+            this.tasks.push(task)
+        },
+        deleteTask(id){
+           this.tasks = this.tasks.filter(t=>{
+             return  t.id !== id
+            })
+        },
+        toggleFav(id){
+            const task = this.tasks.find(task => task.id === id)
+
+            task.isFav = !task.isFav
+        }
+    }
 })
